a17ae57d923817e9837a915b80ff88fcd936351b
prcordova <pedroricardocordova@yahoo.com.br>
2024-04-06 21:53:41 -0300

    fix:adding navigation language default, updating resumes, sidebar & styles sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Avatar from "../img/profile.jpg";
 import SocialNetworks from "./SocialNetworks";
-import cvFile from "../assets/PedroRicardo-resume.pdf";
+import cvFileResume from "../assets/PedroCordova-Resume.pdf";
+import cvFile from "../assets/PedroCordova.pdf";
 
 import "./sidebar.sass";
 import InformationContainer from "./InformationContainer";
 import { useTranslation } from "react-i18next";
 
 const Sidebar = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const currentLanguage = i18n.language;
 
   return (
     <aside id="sidebar">
@@ -17,9 +20,18 @@ const Sidebar = () => {
 
       <SocialNetworks />
       <InformationContainer />
-      <a href={cvFile} download className="btn">
-        {t("download")}
-      </a>
+      <div className="download-area">
+        <a
+          href={currentLanguage === "en" ? cvFileResume : cvFile}
+          download
+          className="btn"
+        >
+          {t("download")}
+        </a>
+        {/* <a href={cvFile} download className="btn">
+          {t("download-pt")}
+        </a> */}
+      </div>
     </aside>
   );
 };
--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -3,6 +3,8 @@ import { initReactI18next } from "react-i18next";
 import en from "../locale/en.json";
 import pt from "../locale/pt.json";
 
+const userLanguage = navigator.language || navigator.userLanguage;
+
 i18n.use(initReactI18next).init({
   resources: {
     en: {
@@ -12,5 +14,5 @@ i18n.use(initReactI18next).init({
       ...pt,
     },
   },
-  lng: "en",
+  lng: userLanguage,
 });
